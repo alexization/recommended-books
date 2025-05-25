087cec3ce921957a0dcb0cd93ff45c2d8d95c58f
hyoseok <alexization@gmail.com>
2025-05-25 23:58:59 +0900

    feat: 유저 등록 route 추가

--- src/routes/UserRoutes.js
+++ src/routes/UserRoutes.js
@@ -3,4 +3,9 @@ import {userController} from "../controllers/UserController.js";
 
 const userRouter = new Router();
 
+/* 회원 등록 */
+userRouter.post('/users', async (req, res) => {
+    await userController.createUser(req, res);
+})
+
 export default userRouter;
