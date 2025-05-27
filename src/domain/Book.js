acc2491c845ac8ca86a7fb6fe8a971d9de66db30
hyoseok <alexization@gmail.com>
2025-05-27 13:00:44 +0900

    feat: Open API 변경으로 인한 Book 모델 속성값 변경

--- src/domain/Book.js
+++ src/domain/Book.js
@@ -1,22 +1,16 @@
 export class Book {
     constructor(bookData) {
-        this.title = bookData.title || '';
-        this.alternativeTitle = bookData.alternativeTitle || '';
-        this.creator = bookData.creator || '';    // 주된 책임을 진 개체
-        this.regDate = bookData.regDate || '';   // 등록일
-        this.collectionDb = bookData.collectionDb || '';  // 소속 DB
-        this.subjectCategory = bookData.subjectCategory || '';
-        this.subjectKeyword = bookData.subjectKeyword || '';
-        this.extent = bookData.extent || '';
-        this.description = bookData.description || '';
-        this.spatialCoverage = bookData.spatialCoverage || '';
-        this.temporal = bookData.temporal || '';
-        this.person = bookData.person || '';
-        this.language = bookData.language || '';
-        this.sourceTitle = bookData.sourceTitle || '';
-        this.rights = bookData.rights || '';
-        this.copyrightOthers = bookData.copyrightOthers || '';
-        this.contributor = bookData.contributor || '';
+        this.bk_nm = bookData.bk_nm;    // 도서명
+        this.aut_nm = bookData.aut_nm;  // 저자명
+        this.pblshr = bookData.pblshr;  // 출판사
+        this.pblcn_yr = bookData.pblcn_yr;  // 발행연도
+        this.callno = bookData.callno;  // 청구기호
+        this.lib = bookData.lib;    // 소장 도서관
+        this.refrm = bookData.refrm;    // 소장 자료실
+        this.loan_yn = bookData.loan_yn;    // 대출여부
+        this.rtn_ed = bookData.rtn_ed;  // 반납 예정일
+        this.bk_rsvt = bookData.bk_rsvt;    // 도석예약 여부
+        this.mutl_loan = bookData.mutl_loan;    // 상호대차 여부
     }
 
     static fromJSON(bookData) {
@@ -25,23 +19,17 @@ export class Book {
 
     toJSON() {
         return {
-            title: this.title,
-            alternativeTitle: this.alternativeTitle,
-            creator: this.creator,
-            regDate: this.regDate,
-            collectionDb: this.collectionDb,
-            subjectCategory: this.subjectCategory,
-            subjectKeyword: this.subjectKeyword,
-            extent: this.extent,
-            description: this.description,
-            spatialCoverage: this.spatialCoverage,
-            temporal: this.temporal,
-            person: this.person,
-            language: this.language,
-            sourceTitle: this.sourceTitle,
-            rights: this.rights,
-            copyrightOthers: this.copyrightOthers,
-            contributor: this.contributor,
+            bk_nm: this.bk_nm,
+            aut_nm: this.aut_nm,
+            pblshr: this.pblshr,
+            pblcn_yr: this.pblcn_yr,
+            callno: this.callno,
+            lib: this.lib,
+            refrm: this.refrm,
+            loan_yn: this.loan_yn,
+            rtn_ed: this.rtn_ed,
+            bk_rsvt: this.bk_rsvt
+            mutl_loan: this.mutl_loan
         };
     }
 }
