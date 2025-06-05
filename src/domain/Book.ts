b97b1309e2d776fd13dafe5fd4276d6b0b18b814
hyoseok <alexization@gmail.com>
2025-06-05 09:21:00 +0900

    feat: BookDto.ts 생성

--- src/domain/Book.ts
+++ src/domain/Book.ts
@@ -1,16 +1,4 @@
-export interface BookData {
-    bk_nm: string;
-    aut_nm: string;
-    pblshr: string;
-    pblcn_yr: string;
-    callno: string;
-    lib: string;
-    refrm: string;
-    loan_yn: string;
-    rtn_ed: string;
-    bk_rsvt: string;
-    mutl_loan: string;
-}
+import {BookData} from "./dto/BookDto";
 
 export class Book {
     public bk_nm: string;
