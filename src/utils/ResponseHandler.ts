b20858ee1aaaeec7bd58b217d2b6a22811885835
hyoseok <alexization@gmail.com>
2025-06-05 11:29:22 +0900

    feat: Context 기반 미들웨어 에러 핸들링 개선 및 공통 응답처리 개선

--- src/utils/ResponseHandler.ts
+++ src/utils/ResponseHandler.ts
@@ -1,15 +1,17 @@
-import {ServerResponse} from "http";
+import {Context} from "koa";
 
 export class ResponseHandler {
-    static success<T>(res: ServerResponse, message: string, data: T): void {
-        res.end(JSON.stringify({
-            status: 'success', message: message, data
-        }));
+    static success<T>(ctx: Context, message: string, data: T): void {
+        ctx.status = 200;
+        ctx.body = {
+            status: 'success', message: message, data: data,
+        };
     }
 
-    static error(res: ServerResponse, message: string, statusCode: number): void {
-        res.end(JSON.stringify({
-            status: 'error', message: message, statusCode
-        }));
+    static error(ctx: Context, message: string, statusCode: number): void {
+        ctx.status = statusCode;
+        ctx.body = {
+            status: 'error', message: message,
+        };
     }
 }
