baa8cb90a62fb454fb754237dece450d7332ce46
hyoseok <alexization@gmail.com>
2025-06-05 11:36:40 +0900

    사용자 컨트롤러 Koa의 Context 적용
    # Conflicts:
    #	src/controllers/UserController.ts
    
    # Conflicts:
    #	src/controllers/UserController.ts

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,9 +1,8 @@
+import {Context} from "koa";
 import {userService} from "../services/UserService";
-import {BadRequestError, ValidationError} from "../utils/AppError";
+import {ValidationError} from "../utils/AppError";
 import {ResponseHandler} from "../utils/ResponseHandler";
 import {UserServiceInterface} from "../interfaces/UserServiceInterface";
-import {ServerResponse} from "http";
-import {UserRequest} from "../requests/UserRequest";
 import {UpdateUserData} from "../domain/dto/UserDto";
 
 export class UserController {
@@ -11,69 +10,52 @@ export class UserController {
         this.userService = userService;
     }
 
-    async createUser(req: UserRequest.CreateRequest, res: ServerResponse): Promise<void> {
+    async createUser(ctx: Context): Promise<void> {
+        this.validateEmail(ctx.request.body.email);
+        this.validateName(ctx.request.body.name);
 
-        if (!req.body) {
-            throw new BadRequestError("생성 데이터가 없습니다.");
-        }
+        const newUser = await this.userService.createUser({...ctx.request.body});
 
-        this.validateEmail(req.body.email);
-        this.validateName(req.body.name);
-
-        const newUser = await this.userService.createUser({...req.body});
-
-        ResponseHandler.success(res, '사용자가 정상적으로 등록되었습니다.', newUser);
+        ResponseHandler.success(ctx, '사용자가 정상적으로 등록되었습니다.', newUser);
     }
 
-    async findUserById(req: UserRequest.FindByIdRequest, res: ServerResponse): Promise<void> {
+    async findUserById(ctx: Context): Promise<void> {
+        const id = parseInt(ctx.params.id);
 
-        if (!req.params) {
-            throw new BadRequestError("조회 데이터가 없습니다.");
-        }
+        const user = await this.userService.findUserById(id);
 
-        const user = await this.userService.findUserById(req.params.id);
-
-        ResponseHandler.success(res, '사용자 정보를 성공적으로 가져왔습니다.', user);
+        ResponseHandler.success(ctx, '사용자 정보를 성공적으로 가져왔습니다.', user);
     }
 
-    async findUserByEmail(req: UserRequest.FindByEmailRequest, res: ServerResponse): Promise<void> {
+    async findUserByEmail(ctx: Context): Promise<void> {
+        const email = ctx.request.body.email as string;
 
-        if (!req.query) {
-            throw new BadRequestError("조회 데이터가 올바르지 않습니다.");
-        }
+        this.validateEmail(email);
 
-        this.validateEmail(req.query.email);
+        const user = await this.userService.findUserByEmail(email);
 
-        const user = await this.userService.findUserByEmail(req.query.email);
-
-        ResponseHandler.success(res, '사용자 정보를 성공적으로 가져왔습니다.', user);
+        ResponseHandler.success(ctx, '사용자 정보를 성공적으로 가져왔습니다.', user);
     }
 
-    async updateUser(req: UserRequest.UpdateRequest, res: ServerResponse): Promise<void> {
+    async updateUser(ctx: Context): Promise<void> {
+        const id = parseInt(ctx.params.id);
 
-        if (!req.params) {
-            throw new BadRequestError("조회 데이터가 없습니다.");
-        }
+        const updateUserData: UpdateUserData = {...ctx.request.body};
 
-        const updateUserData: UpdateUserData = {...req.body};
+        await this.userService.updateUser(id, updateUserData);
 
-        await this.userService.updateUser(req.params.id, updateUserData);
-
-        ResponseHandler.success(res, '사용자 정보를 성공적으로 수정했습니다.', null);
+        ResponseHandler.success(ctx, '사용자 정보를 성공적으로 수정했습니다.', null);
     }
 
-    async deleteUser(req: UserRequest.DeleteRequest, res: ServerResponse): Promise<void> {
+    async deleteUser(ctx: Context): Promise<void> {
+        const id = parseInt(ctx.params.id);
 
-        if (!req.params) {
-            throw new BadRequestError("조회 데이터가 없습니다.");
-        }
+        await this.userService.deleteUser(id);
 
-        await this.userService.deleteUser(req.params.id);
-
-        ResponseHandler.success(res, '사용자를 성공적으로 삭제했습니다.', null);
+        ResponseHandler.success(ctx, '사용자를 성공적으로 삭제했습니다.', null);
     }
 
-    validateEmail(email: string): void {
+    validateEmail(email?: string): void {
         if (!email || email.trim() === '') {
             throw new ValidationError("이메일은 필수 입력 사항입니다.");
         }
@@ -84,7 +66,7 @@ export class UserController {
         }
     }
 
-    validateName(name: string): void {
+    validateName(name?: string): void {
         if (!name || name.trim() === '') {
             throw new ValidationError("이름은 필수 입력 사항입니다.");
         }
