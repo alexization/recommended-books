ff98559cb332749c7f6c0fdb772557e1f2cb02f5
hyoseok <alexization@gmail.com>
2025-06-05 11:29:22 +0900

    feat: Context 기반 미들웨어 에러 핸들링 개선 및 공통 응답처리 개선

--- src/middlewares/ErrorHandlerMiddleware.ts
+++ src/middlewares/ErrorHandlerMiddleware.ts
@@ -1,28 +1,27 @@
+import {Context, Next} from "koa";
 import {AppError, BadRequestError, NotFoundError, ValidationError} from "../utils/AppError";
 import {ResponseHandler} from "../utils/ResponseHandler";
-import {IncomingMessage, ServerResponse} from "http";
-import {NextFunction} from "./MiddlewareManager";
 
-export const errorHandlerMiddleware = async (req: IncomingMessage, res: ServerResponse, next: NextFunction) => {
+export const errorHandlerMiddleware = async (ctx: Context, next: Next): Promise<void> => {
     try {
         await next();
     } catch (error: unknown) {
         if (error instanceof ValidationError) {
-            return ResponseHandler.error(res, error.message, 400);
+            return ResponseHandler.error(ctx, error.message, 400);
         }
 
         if (error instanceof NotFoundError) {
-            return ResponseHandler.error(res, error.message, 404);
+            return ResponseHandler.error(ctx, error.message, 404);
         }
 
         if (error instanceof BadRequestError) {
-            return ResponseHandler.error(res, error.message, 400);
+            return ResponseHandler.error(ctx, error.message, 400);
         }
 
         if (error instanceof AppError) {
-            return ResponseHandler.error(res, error.message, 400);
+            return ResponseHandler.error(ctx, error.message, 400);
         }
 
-        return ResponseHandler.error(res, '예상치 못한 오류 입니다.', 500);
+        return ResponseHandler.error(ctx, '예상치 못한 오류 입니다.', 500);
     }
 };
