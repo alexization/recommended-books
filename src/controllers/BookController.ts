b04da4e703c6095c60aa249bd8eb239778040eb3
hyoseok <alexization@gmail.com>
2025-06-05 11:39:04 +0900

    feat: 도서 컨트롤러 Koa Context 적용

--- src/controllers/BookController.ts
+++ src/controllers/BookController.ts
@@ -1,26 +1,24 @@
+import {Context} from "koa";
 import {bookService} from "../services/BookService";
 import {ResponseHandler} from "../utils/ResponseHandler";
 import {ValidationError} from "../utils/AppError";
 import {BookServiceInterface} from "../interfaces/BookServiceInterface";
-import {ServerResponse} from "http";
-import {BookRequest} from "../requests/BookRequest";
 
 export class BookController {
     constructor(private readonly bookService: BookServiceInterface) {
         this.bookService = bookService;
     }
 
-    async getBooks(req: BookRequest, res: ServerResponse): Promise<void> {
-        const pageNo = req.query?.pageNo!;
-        const numberPageNo = Number(pageNo);
+    async getBooks(ctx: Context): Promise<void> {
+        const pageNo = parseInt(ctx.query.pageNo as string);
 
-        if (isNaN(numberPageNo) || numberPageNo < 1) {
+        if (isNaN(pageNo) || pageNo < 1) {
             throw new ValidationError('페이지 번호는 1 이상이어야 합니다.');
         }
 
         const bookData = await this.bookService.getBooks(pageNo);
 
-        ResponseHandler.success(res, `${pageNo}번 페이지 도서를 성공적으로 가져왔습니다.`, bookData);
+        ResponseHandler.success(ctx, `${pageNo}번 페이지 도서를 성공적으로 가져왔습니다.`, bookData);
     }
 }
 
