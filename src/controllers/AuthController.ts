d2ae3bbdfa2e0806c24cfc6b82b080a12f219d98
hyoseok <alexization@gmail.com>
2025-06-10 17:45:13 +0900

    feat: 로그인을 위한 AuthController 코드 구현

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,22 @@
+import {AuthServiceInterface} from "../interfaces/AuthServiceInterface";
+import {authService} from "../services/AuthService";
+import {Context} from "koa";
+import {LoginUserData} from "../domain/dto/UserDto";
+import {ResponseHandler} from "../utils/ResponseHandler";
+
+export class AuthController {
+
+    constructor(private readonly authService: AuthServiceInterface) {
+        this.authService = authService;
+    }
+
+    login = async (ctx: Context): Promise<void> => {
+        const loginUserData = ctx.request.body as LoginUserData;
+
+        const tokens = await this.authService.login(loginUserData);
+
+        ResponseHandler.success(ctx, "성공적으로 로그인했습니다.", tokens);
+    };
+}
+
+export const authController = new AuthController(authService);
