b55e74c6fb39b578780a98cabb6a57944a32729e
hyoseok <alexization@gmail.com>
2025-06-24 14:45:57 +0900

    feat: 게시물 등록 API 명세서 작성

--- src/domain/dto/PostDto.ts
+++ src/domain/dto/PostDto.ts
@@ -8,6 +8,28 @@ export interface PostData {
     imagePath?: string;
 }
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *      CreatePostData:
+ *          type: object
+ *          required:
+ *              - title
+ *              - content
+ *          properties:
+ *              title:
+ *                  type: string
+ *                  example: "무지개 물고기 독후감"
+ *              content:
+ *                  type: string
+ *                  example: "무지개 물고기 독후감 내용을 적습니다."
+ *              bookId:
+ *                  type: integer
+ *                  example: 1
+ *              imageBase64:
+ *                  type: string
+ * */
 export interface CreatePostData {
     title: string;
     content: string;
--- src/routes/PostRoutes.ts
+++ src/routes/PostRoutes.ts
@@ -5,6 +5,22 @@ const postRouter = new Router({
     prefix: '/posts',
 });
 
+/**
+ * @swagger
+ * /posts:
+ *  post:
+ *      summary: 게시물 등록
+ *      tags: [Posts]
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      $ref: '#/components/schemas/CreatePostData'
+ *      responses:
+ *          200:
+ *              description: 성공
+ * */
 postRouter.post('/', postController.createPost);
 
 export default postRouter;
