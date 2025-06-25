3d975cc9565c4bd34d835c50bf21468bfe96bf2a
hyoseok <alexization@gmail.com>
2025-06-25 11:33:17 +0900

    feat: Password 값 객체 생성 및 구현

---
+++ src/domain/valueObjects/Password.ts
@@ -0,0 +1,37 @@
+import {ValidationError} from "../../exception/AppError";
+import {ErrorMessage} from "../../exception/ErrorMessage";
+import bcrypt from "bcryptjs";
+
+export class Password {
+    private readonly value: string;
+
+    constructor(password: string, isHashed: boolean) {
+        if (!isHashed) {
+            this.validatePassword(password);
+        }
+        this.value = password;
+    }
+
+    async hash(): Promise<Password> {
+        const hashedValue = await bcrypt.hash(this.value, 10);
+        return new Password(hashedValue, true);
+    }
+
+    async matches(plainPassword: string): Promise<boolean> {
+        return await bcrypt.compare(plainPassword, this.value);
+    }
+
+    getValue(): string {
+        return this.value;
+    }
+
+    private validatePassword(password: string) {
+        if (!password) {
+            throw new ValidationError(ErrorMessage.PASSWORD_REQUIRED);
+        }
+
+        if (password.length < 6 || password.length > 12) {
+            throw new ValidationError(ErrorMessage.PASSWORD_INVALID_LENGTH);
+        }
+    }
+}
